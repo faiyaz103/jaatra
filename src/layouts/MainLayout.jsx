9ef74ff6c8330865c96146fddad4403161b21764
Faiyaz Mahmud <faiyazmahmud2002@gmail.com>
2024-12-20 22:58:16 +0600

    Navigation Bar Added

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navigation from '../components/Navigation'
+
+const MainLayout = () => {
+  return (
+    <>
+    <Navigation/>
+    </>
+  )
+}
+
+export default MainLayout
